61bf500f1ccb959d779f90055a0c5e6fb4754923
JuanCamiloMontoya <jhonkey@outlook.es>
2022-10-24 22:17:11 -0500

    refactor(common): models folder created

---
+++ src/@common/models/objects/error.objects.ts
@@ -0,0 +1,6 @@
+import { Error500Response } from "../types/error.types"
+
+export const Error500Options = {
+  type: Error500Response,
+  description: 'Error interno'
+}
---
+++ src/@common/models/types/error.types.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger"
+
+class ErrorResponse {
+
+  @ApiProperty({
+    example: 'Error X'
+  })
+  message: String
+
+}
+
+export class Error500Response extends ErrorResponse {
+
+  @ApiProperty({
+    example: 500,
+    type: Number
+  })
+  statusCode: Number
+
+}
+
+export class Error400Response extends ErrorResponse {
+
+  @ApiProperty({
+    example: 400,
+    type: Number
+  })
+  statusCode: Number
+
+}
